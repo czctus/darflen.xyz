058a2641ad8a425e52df9c2cdf973a722269d87c
czctus <czc@perox.dev>
2026-07-17 12:07:33 -0400

    fix(utils): markdown should remove all elements now

--- src/utils.ts
+++ src/utils.ts
@@ -6,17 +6,24 @@ export const darflen = new DarflenClient();
 
 export function clean(str: string) {
 	return str
+		// html
+
 		.replaceAll('&', "&amp;") // replaces &
 		.replaceAll('<', "&lt;") // replaces <
 		.replaceAll('>', "&gt;") // replaces >
 		.replaceAll('"', "&quot;") // replaces "
 		.replaceAll("'", "&#39;") // replaces '
+
+		// md
+
+		.replaceAll(/\|\|(.+?)\|\|/g, '$1') // removes ||spoiler||
+		.replaceAll(/__(.+?)__/g, '$1') // removes __underline__
 		.replaceAll(/\*\*(.+?)\*\*/g, '$1') // removes **bold**
 		.replaceAll(/\*(.+?)\*/g, '$1') // removes *italics*
 		.replaceAll(/~~(.+?)~~/g, '$1') // removes ~~strikethrough~~
-		.replaceAll(/`(.+?)`/g, '$1') // removes `inline code`
-		.replaceAll(/```[\s\S]*?```/g, '') // removes ```code blocks```
-		.replaceAll(/^-?#+\s*/gm, '') // removes markdown headers like #, ##, -#, -## (with or without space)
+		.replaceAll(/\^(.+?)\^/g, '$1') // removes ^quote^
+		.replaceAll(/`{1,3}([\s\S]*?)`{1,3}/g, '$1') // removes `inline code` and ```code blocks```
+		.replaceAll(/^#+\s*/gm, '') // removes markdown headers like #, ##, etc
 		.replace(/!\[(.*?)\]\(.*?\)/g, '$1') // replaces ![alt text](url) with alt text
 		.replaceAll(/\[(.+?)\]\(.*?\)/g, '$1'); // replaces [text](url) with text
 }
@@ -46,13 +53,15 @@ export function getUserProps(profile: Profile) {
 	const image = profile.media.avatar.data.large || profile.media.avatar.data.medium || profile.media.avatar.data.thumbnail;
 	const banner = profile.media.banner.data.large || profile.media.banner.data.medium || profile.media.banner.data.thumbnail;
 
-	return { title, image: {
-		url: image,
-		data: profile.media.avatar
-	}, banner: {
-		url: banner,
-		data: profile.media.banner
-	}  };
+	return {
+		title, image: {
+			url: image,
+			data: profile.media.avatar
+		}, banner: {
+			url: banner,
+			data: profile.media.banner
+		}
+	};
 }
 
 export function getDescription(tags: string, content: string) {
